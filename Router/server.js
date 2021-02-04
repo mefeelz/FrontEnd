ffeed93a81a81104414f11b7fe0af81e5d3fe84b
gimkuku <52443092+gimkuku@users.noreply.github.com>
2021-02-04 19:07:46 +0900

    summernote

--- Router/server.js
+++ Router/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const Router = require('express-router');
-var expressLayouts = require('express-ejs-layouts');
+                     // express-ejs-layouts 사용하기 설정
 
 const app = express();
 const router = express.Router();
 const port = 3000;
 
-app.set('view engine', 'ejs');                  // express 의 view 엔진을 ejs 로 세팅 
-app.set('views', __dirname + '/views');         // 디폴트 view 경로세팅 
-app.use(expressLayouts);                        // express-ejs-layouts 사용하기 설정
+app.set('views','views'); // 정적 파일 위치 지정
+app.set('view engine','ejs'); // 템플릿 엔진 세팅
 
 router.get('/',(req,res,next) => {
   res.render('main.ejs');
   next();
 });
+
 
 app.use('/',router);
 
