d27ca3c41a6f7a78917400c6dc7f00f6ef5893d9
gimkuku <52443092+gimkuku@users.noreply.github.com>
2021-02-04 19:57:59 +0900

    login page

--- Router/server.js
+++ Router/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const Router = require('express-router');
                      // express-ejs-layouts 사용하기 설정
-
 const app = express();
 const router = express.Router();
 const port = 3000;
@@ -11,6 +10,12 @@ app.set('view engine','ejs'); // 템플릿 엔진 세팅
 
 router.get('/',(req,res,next) => {
   res.render('main.ejs');
+  next();
+});
+
+// localhost:3000/login
+router.get('/login',(req,res,next) => {
+  res.render('login.ejs');
   next();
 });
 
