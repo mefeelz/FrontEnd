1a35c5c65b397417881b61872d6337e9a745fa46
gimkuku <52443092+gimkuku@users.noreply.github.com>
2021-02-05 19:25:17 +0900

    search

--- Router/server.js
+++ Router/server.js
@@ -10,7 +10,7 @@ app.set('view engine','ejs'); // 템플릿 엔진 세팅
 app.use(express.static('statics'));
 
 //One of the routes
-router.get("/", (req, res) => {
+router.get("/main", (req, res) => {
   fetch("http://localhost:3000/api/main/qna") 
       .then(response => response.json())
       .then(qnadata => {
@@ -30,15 +30,37 @@ router.get('/login',(req,res,next) => {
   res.render('login.ejs');
   next();
 });
+router.get("/main/search", (req, res) => {
+  var keyword = req.query.keyword;
+  console.log("keyord"+keyword);
+  fetch("http://localhost:3000/api/main/search?keyword=12") 
+      .then(response => response.json())
+      .then(answer => {
+        return console.log("answer"+answer);
+        // return res.render("aftersearch.ejs", { answer: answer});
+        })
+      .catch(err => {
+        console.log("err"+err);
+      });
+});
 
 // localhost:3000/login
 router.get('/review/:uid',(req,res,next) => {
   var uid = req.params.uid;
-  fetch("http://localhost:3000/api/div/info?uid="+uid,{method:'GET'}) 
-  .then(response => response.json())
+  var type = 1 || req.query.type;
+  var status = 2 || req.query.status;
+  fetch("http://localhost:3000/api/div/info?uid=1",{method: 'GET'}) 
+  .then(response => {console.log(response); return response.json();})
   .then(divdata => {
-    return res.render("review.ejs", { divdata: divdata});
-  }).catch(err => {
+    console.log(divdata);
+    console.log(err);
+    fetch("http://localhost:3000/api/div/rating?uid="+uid+"&type="+type+"&status="+status,{method:'GET'}) 
+    .then(response => response.body.json())
+    .then(ratingdata => {
+      return res.render("review.ejs", { divdata: divdata , ratingdata: ratingdata,});
+    });
+  })
+  .catch(err => {
     console.log("err"+err);
   });
 });
